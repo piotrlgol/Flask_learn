57d9017891e1b0db1a6814290949607afa782aa5
Piotr Golonko <piopiotrek008@gmail.com>
2019-05-22 14:24:52 +0200

    html rendering templates; user using bootstrap

--- App.py
+++ App.py
@@ -1,16 +1,17 @@
 from flask import Flask
-from flask import request
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug = True)
