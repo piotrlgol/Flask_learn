635b7971be60ebb5cc74990695ef20a34f2b579d
Piotr Golonko <piopiotrek008@gmail.com>
2019-05-30 12:53:53 +0200

    deleted time display

--- App.py
+++ App.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -17,7 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'This Particular String'
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -33,7 +30,7 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known',False), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -46,6 +43,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
