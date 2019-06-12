54b0c1fb260598c0490f7e4e37d891c76e8756ea
Piotr Golonko <piopiotrek008@gmail.com>
2019-06-12 10:17:55 +0200

    test for user password

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,7 @@
 import unittest
 from flask import current_app
 from app import create_app, db
+from app.models import User
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
@@ -19,4 +20,22 @@ class BasicsTestCase(unittest.TestCase):
 
     def test_app_is_testint(self):
         self.assertTrue(current_app.config['TESTING'])
-            
+
+class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.u = User(password = 'cat')
+
+    def test_password_setter(self):
+        self.assertTrue(self.u.password_hash is not None)
+
+    def test_no_password_getter(self):
+        with self.assertRaises(AttributeError):
+            self.u.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.u.verify_password('cat'))
+        self.assertFalse(self.u.verify_password('dog'))
+
+    def test_passwords_salts_are_random(self):
+        u2 = User(password = 'cat')
+        self.assertTrue(self.u.password_hash != u2.password_hash)            
