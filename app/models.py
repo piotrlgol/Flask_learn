2fc647b67dd671d7e0bf42c3ac72a4297b7d06af
Piotr Golonko <piopiotrek008@gmail.com>
2019-06-12 10:06:11 +0200

    fixed password_hashing

--- app/models.py
+++ app/models.py
@@ -21,9 +21,9 @@ class User(db.Model):
     def password(self):
         raise AttributeError('password is not a readable attribute')
 
-    @property
+    @password.setter
     def password(self, password):
-        self.password_hash  =generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
